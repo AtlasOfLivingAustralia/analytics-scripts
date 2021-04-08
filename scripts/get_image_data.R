a907139c6abd1d57709ed97a65bb95374870af36
matilda2812 <matildastevenson2812@gmail.com>
2021-04-08 14:18:05 +1000

    Backup image data to S3

--- scripts/get_image_data.R
+++ scripts/get_image_data.R
@@ -8,6 +8,23 @@ sp_image_count <- fromJSON("https://biocache.ala.org.au/ws/occurrence/facets?q=m
 
 today <- format(Sys.time(), format="%Y%m%d%H%M")
 
+
+# Write to a temporary file
+tmp <- tempfile()
+out_path <- paste0("image_counts/", today, ".json")
+
 write_json(data.frame(image_count = image_count,
                       taxa_image_count = taxa_image_count,
-                      species_image_count = sp_image_count), path = paste0("/data/daily_image_stats/",today,".json"))
+                      species_image_count = sp_image_count), path = tmp)
+
+# Send file to S3
+bucket_id <- Sys.getenv("AWS_BUCKET_ID")
+
+# Create an object to interact with S3
+s3 <- paws::s3()
+
+s3$put_object(
+  Bucket = bucket_id,
+  Body = tmp,
+  Key = out_path
+)
