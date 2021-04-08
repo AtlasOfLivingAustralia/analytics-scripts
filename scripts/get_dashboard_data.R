d670a53279a661f726fc960f1f64dc98cb34b553
matilda2812 <matildastevenson2812@gmail.com>
2021-04-08 14:09:13 +1000

    Backup to S3 directly

--- scripts/get_dashboard_data.R
+++ scripts/get_dashboard_data.R
@@ -1,8 +1,25 @@
 library(httr)
 library(jsonlite)
+library(paws)
 
 # grab the data and store it locally
 today <- format(Sys.time(), format="%Y%m%d%H%M")
 get <- GET("https://dashboard.ala.org.au/dashboard/data")
 content <- fromJSON(content(get,as="text"))
-write_json(content,paste0("/data/daily_downloads/",today,".json"))
+
+# Write to a temporary file
+tmp <- tempfile()
+out_path <- paste0("dashboard_data/", today, ".json")
+write_json(content,tmp)
+
+# Send file to S3
+bucket_id <- Sys.getenv("AWS_BUCKET_ID")
+
+# Create an object to interact with S3
+s3 <- paws::s3()
+
+s3$put_object(
+  Bucket = bucket_id,
+  Body = tmp,
+  Key = out_path
+)
