431d27db718868a7822bc5935a7f736ff7d54453
matilda2812 <matildastevenson2812@gmail.com>
2021-04-08 14:41:26 +1000

    Backup data resource counts to s3

--- scripts/get_dataresource_counts.R
+++ scripts/get_dataresource_counts.R
@@ -7,7 +7,19 @@ library(galah)
 dr_counts <- ala_counts(group_by = 'data_resource_uid', limit = 10000)
 dr_counts$date <- Sys.Date()
 
-file <- paste0(as.character(Sys.Date()), "_dr_counts.csv")
+# Write to a temporary file
+tmp <- tempfile()
+out_path <- paste0("data_resource_counts/", Sys.Date(), "_dr_counts.csv")
+write.csv(dr_counts,tmp, row.names = FALSE)
 
-write.csv(dr_counts, file.path('/data/data_resource_counts', file),
-                                row.names = FALSE)
+# Send file to S3
+bucket_id <- Sys.getenv("AWS_BUCKET_ID")
+
+# Create an object to interact with S3
+s3 <- paws::s3()
+
+s3$put_object(
+  Bucket = bucket_id,
+  Body = tmp,
+  Key = out_path
+)
